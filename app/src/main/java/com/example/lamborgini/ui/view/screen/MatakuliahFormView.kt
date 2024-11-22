eabd2185e64bca1fe3f003ccf71282a02860c7f3
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2024-11-22 20:13:19 +0700

    Menambahkan Header Section dengan logo dan nama universitas

--- app/src/main/java/com/example/lamborgini/ui/view/screen/MatakuliahFormView.kt
+++ app/src/main/java/com/example/lamborgini/ui/view/screen/MatakuliahFormView.kt
@@ -1,15 +1,29 @@
 package com.example.lamborgini.ui.view.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lamborgini.R
 import com.example.lamborgini.ui.model.Mahasiswa
 
@@ -26,5 +40,33 @@ fun MatakuliahView(
         modifier = Modifier
             .fillMaxSize()
             .background(color = colorResource(id = R.color.primary))
-    ){}
+    ){
+        Row(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.harvard),
+                contentDescription = "Logo",
+                modifier = Modifier.size(80.dp)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Column {
+                Text(
+                    text = "HARVARD UNIVERSITY",
+                    color = Color.Red,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = "Muda Mendunia UHUYYYYYY",
+                    color = Color.White,
+                    fontSize = 14.sp
+                )
+            }
+        }
+
+    }
 }
