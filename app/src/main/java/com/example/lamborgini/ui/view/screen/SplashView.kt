8366cc8e14d8dcfefe8a71117ac68c2a6d6626ac
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2024-11-22 19:47:21 +0700

    Menambahkan Spacer dan tombol Mulai untuk navigasi awal aplikasi

--- app/src/main/java/com/example/lamborgini/ui/view/screen/SplashView.kt
+++ app/src/main/java/com/example/lamborgini/ui/view/screen/SplashView.kt
@@ -4,8 +4,13 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -37,7 +42,12 @@ fun SplashView(
             contentDescription = "",
             modifier = Modifier.size(250.dp)
         )
-
+        Spacer(modifier = Modifier.padding(16.dp))
+        Button(onClick = {onMulaiButton()}, modifier = Modifier
+            .fillMaxWidth()
+            .padding(32.dp)) {
+            Text(text = "Mulai")
+        }
     }
 }
 
