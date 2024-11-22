0e0efdd4e16a973ffb5471c497c4c98fd21bcdd9
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2024-11-22 20:16:01 +0700

    Menambahkan Section untuk menampilkan data mahasiswa

--- app/src/main/java/com/example/lamborgini/ui/view/screen/MatakuliahFormView.kt
+++ app/src/main/java/com/example/lamborgini/ui/view/screen/MatakuliahFormView.kt
@@ -2,6 +2,7 @@ package com.example.lamborgini.ui.view.screen
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -40,7 +42,7 @@ fun MatakuliahView(
         modifier = Modifier
             .fillMaxSize()
             .background(color = colorResource(id = R.color.primary))
-    ){
+    ) {
         Row(
             modifier = Modifier
                 .padding(16.dp)
@@ -67,6 +69,29 @@ fun MatakuliahView(
                 )
             }
         }
+        Box(
+            modifier = Modifier
+                .background(
+                    color = Color.White,
+                    shape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp)
+                )
+                .fillMaxSize()
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(24.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                // Mahasiswa Data
+                Text(
+                    text = "Data Mahasiswa",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black
+                )
 
+            }
+        }
     }
 }
