92cc9aa6d3d32992471736c1e70a915d5e74de07
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2024-11-22 20:26:09 +0700

    Menambahkan MahasiswaViewModel untuk mengelola data mahasiswa

--- app/src/main/java/com/example/lamborgini/ui/view/viewmodel/MahasiswaViewModel.kt
+++ app/src/main/java/com/example/lamborgini/ui/view/viewmodel/MahasiswaViewModel.kt
@@ -1,2 +1,27 @@
 package com.example.lamborgini.ui.view.viewmodel
 
+import androidx.lifecycle.ViewModel
+import com.example.lamborgini.ui.model.Mahasiswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class MahasiswaViewModel : ViewModel() {
+
+    //request
+    private val _statusUI = MutableStateFlow(Mahasiswa())
+
+    //Response
+    val statusUI: StateFlow<Mahasiswa> = _statusUI.asStateFlow()
+
+    fun saveDataSiswa(ls: MutableList<String>) {
+        _statusUI.update { statusSaatIni ->
+            statusSaatIni.copy(
+                nim = ls[0],
+                nama = ls[1],
+                email = ls[2]
+            )
+        }
+    }
+}
