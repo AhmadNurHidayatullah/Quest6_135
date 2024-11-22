7cef0170d95b5b554f1bf2578205af9f0bdecc6e
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2024-11-22 20:08:42 +0700

    Menambahkan view Matakuliah dengan state untuk mata kuliah dan kelas

--- app/src/main/java/com/example/lamborgini/ui/view/screen/MatakuliahFormView.kt
+++ app/src/main/java/com/example/lamborgini/ui/view/screen/MatakuliahFormView.kt
@@ -1,2 +1,18 @@
 package com.example.lamborgini.ui.view.screen
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import com.example.lamborgini.ui.model.Mahasiswa
+
+@Composable
+fun MatakuliahView(
+    uiState: Mahasiswa,
+    onSimpanButtonClicked: (MutableList<String>) -> Unit
+) {
+    var mataKuliah by remember { mutableStateOf("") }
+    var selectedKelas by remember { mutableStateOf("") }
+    val listData: MutableList<String> = mutableListOf()
+}
