f168f62a166062ed00cd36c242013b7453254b03
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2024-11-22 19:52:21 +0700

    Menambahkan Column dengan latar background warna primary

--- app/src/main/java/com/example/lamborgini/ui/view/screen/MahasiswaFormView.kt
+++ app/src/main/java/com/example/lamborgini/ui/view/screen/MahasiswaFormView.kt
@@ -1,20 +1,37 @@
 package com.example.lamborgini.ui.view.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import com.example.lamborgini.R
 
 @Composable
 fun MahasiswaFormView(
     onSubmitButtonCliked: (MutableList<String>)-> Unit,
     onBackButtonCliked: () -> Unit
-){
+) {
 
     var nim by remember { mutableStateOf("") }
     var nama by remember { mutableStateOf("") }
     var email by remember { mutableStateOf("") }
 
-    val listData: MutableList<String> = mutableListOf(nim,nama,email)
-    }
+    val listData: MutableList<String> = mutableListOf(nim, nama, email)
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                color = colorResource(
+                    id = R.color.primary
+                )
+            ), horizontalAlignment = Alignment.CenterHorizontally
+    ) {}
+}
