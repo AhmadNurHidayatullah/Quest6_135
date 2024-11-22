e75ecb8abe269e52c216618b6978c961d9656b23
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2024-11-22 20:10:36 +0700

    mengubah warna background

--- app/src/main/java/com/example/lamborgini/ui/view/screen/MatakuliahFormView.kt
+++ app/src/main/java/com/example/lamborgini/ui/view/screen/MatakuliahFormView.kt
@@ -1,10 +1,16 @@
 package com.example.lamborgini.ui.view.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import com.example.lamborgini.R
 import com.example.lamborgini.ui.model.Mahasiswa
 
 @Composable
@@ -15,4 +21,10 @@ fun MatakuliahView(
     var mataKuliah by remember { mutableStateOf("") }
     var selectedKelas by remember { mutableStateOf("") }
     val listData: MutableList<String> = mutableListOf()
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(id = R.color.primary))
+    ){}
 }
