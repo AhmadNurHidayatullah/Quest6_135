47b90cf13c5cbd1a97a270134e05d5941b5727e2
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2024-11-22 19:49:46 +0700

    Menambahkan MahasiswaFormView untuk form input data mahasiswa

--- app/src/main/java/com/example/lamborgini/ui/view/screen/MahasiswaFormView.kt
+++ app/src/main/java/com/example/lamborgini/ui/view/screen/MahasiswaFormView.kt
@@ -1,2 +1,20 @@
 package com.example.lamborgini.ui.view.screen
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+@Composable
+fun MahasiswaFormView(
+    onSubmitButtonCliked: (MutableList<String>)-> Unit,
+    onBackButtonCliked: () -> Unit
+){
+
+    var nim by remember { mutableStateOf("") }
+    var nama by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+
+    val listData: MutableList<String> = mutableListOf(nim,nama,email)
+    }
