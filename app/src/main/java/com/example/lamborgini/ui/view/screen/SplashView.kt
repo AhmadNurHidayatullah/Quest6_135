d898070591a9858d944d58590b26cdd5913a3ee7
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2024-11-22 19:43:55 +0700

    Menambahkan SplashView untuk tampilan awal aplikasi

--- app/src/main/java/com/example/lamborgini/ui/view/screen/SplashView.kt
+++ app/src/main/java/com/example/lamborgini/ui/view/screen/SplashView.kt
@@ -1,2 +1,30 @@
 package com.example.lamborgini.ui.view.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import com.example.lamborgini.R
+
+
+@Composable
+fun SplashView(
+    onMulaiButton: () ->Unit,
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                color = colorResource(
+                    id = R.color.primary
+                )
+            ),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){}
+}
+
