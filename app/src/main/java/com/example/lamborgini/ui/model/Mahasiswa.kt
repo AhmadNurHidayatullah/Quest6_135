85556ab55081f74bbf7bf7eb17179b084d52fbf4
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2024-11-22 19:35:41 +0700

    Menambahkan data class Mahasiswa untuk menyimpan informasi mahasiswa

--- app/src/main/java/com/example/lamborgini/ui/model/Mahasiswa.kt
+++ app/src/main/java/com/example/lamborgini/ui/model/Mahasiswa.kt
@@ -1,3 +1,9 @@
 package com.example.lamborgini.ui.model
 
-
+data class Mahasiswa(
+    val nim: String = "",
+    val nama: String = "",
+    val email: String = "",
+    val matakuliah: String = "",
+    val kelas: String = ""
+)
